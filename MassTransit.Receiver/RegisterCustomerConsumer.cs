66c481a91289c373af159cb0b0c67e41ce836727
Carlos Pedro <Carlos.Pedro@immform.org.uk>
2018-02-07 17:00:46 +0000

    first commit

---
+++ MassTransit.Receiver/RegisterCustomerConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Sample.Messages;
+
+namespace MassTransit.Receiver
+{
+    public class RegisterCustomerConsumer : IConsumer<IRegisterCustomer>
+    {
+        public Task Consume(ConsumeContext<IRegisterCustomer> context)
+        {
+            var newCustomer = context.Message;
+            Console.WriteLine("A new customer has signed up, it's time to register it. Details: ");
+            Console.WriteLine(newCustomer.Address);
+            Console.WriteLine(newCustomer.Name);
+            Console.WriteLine(newCustomer.Id);
+            Console.WriteLine(newCustomer.ResgisteredUtc);
+            return Task.FromResult(context.Message);
+
+        }
+    }
+}
---
+++ RabbitMqVanilla/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitMqVanilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            }.CreateConnection();
+
+            var model = connection.CreateModel();
+
+        }
+    }
+}
